8e1f3585ec5aa3c3d349f558fa9f615d0cbee7d5
yk0611 <hello@junkbranding.com>
2024-02-13 15:01:59 +0900

    init処理

--- src/index.js
+++ src/index.js
@@ -1,152 +1,39 @@
-import * as THREE from 'three'
+  const webGl = new WebGLFrame();
 
-import vertex from './assets/shaders/vertex.glsl'
-import fragment from './assets/shaders/fragment.glsl'
+  webGl.init('webgl-canvas');
 
-const canvasEl = document.getElementById('webgl-canvas');
-const canvasSize = {
-  w: window.innerWidth,
-  h: window.innerHeight,
-};
+  webGl.load().then(() => {
+    webGl.setup();
+    webGl.render();
+  })
 
-const renderer = new THREE.WebGLRenderer({ canvas: canvasEl });
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(canvasSize.w, canvasSize.h);
-
-const fov = 60;
-const fovRad = (fov / 2) * (Math.PI / 180);
-const dist = canvasSize.h / 2 / Math.tan(fovRad);
-const camera = new THREE.PerspectiveCamera(
-  fov,
-  canvasSize.w / canvasSize.h,
-  0.1,
-  2000
-);
-camera.position.z = dist;
-
-const scene = new THREE.Scene();
-
-const loader = new THREE.TextureLoader();
-
-class ImagePlane {
-  constructor(mesh, img) {
-    this.refImage = img;
-    this.mesh = mesh;
+class WebGLFrame {
+  constructor() {
+    this.canvas = null;
+    this.gl = null;
+    this.running = false;
+    this.beginTime = 0;
+    this.nowTime = 0;
+    this.render = this.render.bind(this);
   }
 
-  setParams() {
-    const rect = this.refImage.getBoundingClientRect();
+  init(canvas) {
+    if(canvas instanceof HTMLCanvasElement === true) {
+      this.canvas = canvas;
+    } else if(Object.prototype.toString.call(canvas) === '[object String]') {
+      const canvasElm = document.querySelector(`#${canvas}`);
+      if(canvasElm instanceof HTMLCanvasElement === true) {
+        this.canvas = canvasElm;
+      }
+    }
 
-    this.mesh.scale.x = rect.width;
-    this.mesh.scale.y = rect.height;
+    if(this.canvas === null) {
+      throw new Error('Canvas not found');
+    }
 
-    const x = rect.left - canvasSize.w / 2 + rect.width / 2;
-    const y = -rect.top + canvasSize.h / 2 - rect.height / 2;
-    // const x = 0;
-    this.mesh.position.set(x, y, this.mesh.position.z);
-  }
-
-  update(offset) {
-    this.setParams();
-
-    this.mesh.material.uniforms.uTime.value = offset;
+    this.gl = this.canvas.getContext('webgl');
+    if(this.gl === null) {
+      throw new Error('WebGL not supported');
+    }
   }
 }
-
-const createMesh = (img) => {
-  const texture = loader.load(img.src);
-  const disp = loader.load('/assets/images/disp.png')
-
-  const uniforms = {
-    uTexture: { type: 'sampler2D', value: texture },
-    uDisp: { type: 'sampler2D', value: disp },
-    uImageAspect: { type: 'f', value: img.naturalWidth / img.naturalHeight },
-    uPlaneAspect: { type: 'f', value: img.clientWidth / img.clientHeight },
-    uTime: { type: 'f', value: 0 },
-
-  };
-  const geo = new THREE.PlaneGeometry(1, 1, 100, 100); 
-  const mat = new THREE.ShaderMaterial({
-    uniforms,
-    vertexShader: vertex,
-    fragmentShader: fragment
-  });
-
-  const mesh = new THREE.Mesh(geo, mat);
-
-  return mesh;
-};
-
-let targetScrollY = 0;
-let currentScrollY = 0;
-let scrollOffset = 0;
-
-const lerp = (start, end, multiplier) => {
-  return (1 - multiplier) * start + multiplier * end;
-};
-
-const updateScroll = () => {
-
-  targetScrollY = document.documentElement.scrollTop;
-
-  currentScrollY = lerp(currentScrollY, targetScrollY, 0.1);
-
-  scrollOffset = targetScrollY - currentScrollY;
-};
-
-const imagePlaneArray = [];
-
-const loop = () => {
-  updateScroll();
-  for (const plane of imagePlaneArray) {
-    plane.update(scrollOffset);
-  }
-  renderer.render(scene, camera);
-
-  requestAnimationFrame(loop);
-};
-
-const resize = () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  canvasSize.w = width;
-  canvasSize.h = height;
-
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
-
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-
-  const fov = 60;
-  const fovRad = (fov / 2) * (Math.PI / 180);
-  const dist = canvasSize.h / 2 / Math.tan(fovRad);
-  camera.position.z = dist;
-
-  const imageArray = [...document.querySelectorAll('img')];
-  imagePlaneArray.forEach( (plane, i) => {
-    plane.mesh.material.uniforms.uImageAspect.value = imageArray[i].naturalWidth / imageArray[i].naturalHeight
-    plane.mesh.material.uniforms.uPlaneAspect.value = imageArray[i].clientWidth / imageArray[i].clientHeight
-  })
-};
-
-const main = () => {
-  window.addEventListener('load', () => {
-    const imageArray = [...document.querySelectorAll('img')];
-    for (const img of imageArray) {
-      const mesh = createMesh(img);
-      scene.add(mesh);
-
-      const imagePlane = new ImagePlane(mesh, img);
-      imagePlane.setParams();
-
-      imagePlaneArray.push(imagePlane);
-    }
-    loop();
-  });
-
-  window.addEventListener('resize', resize, {passive: true})
-};
-
-main();
